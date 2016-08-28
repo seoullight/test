b0e746429dc4284149ad28f29e25226e2bd78b97
Joonki <jkkim@nextree.co.kr>
2016-08-28 16:21:16 +0900

    change database

--- seoullight/src/seoullight/settings.py
+++ seoullight/src/seoullight/settings.py
@@ -59,8 +59,11 @@ WSGI_APPLICATION = 'seoullight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'seoullight$seoulight',
+        'USER': 'seoullight',
+        'PASSWORD': 'bizadmin',
+        'HOST': 'seoullight.mysql.pythonanywhere-services.com'
     }
 }
 
